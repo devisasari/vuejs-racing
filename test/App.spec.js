c751a8dabfe34f15f798b39f3170dbefb3a38fda
devisasari <isasari.special.mailim@gmail.com>
2022-10-30 23:30:12 +0300

    add vitest
    I tried to test the App.vue component, but I didn't get it to work. I will learn how to test components with vitest and test the App.vue component later.

---
+++ test/App.spec.js
@@ -0,0 +1,6 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import App from "../src/App.vue";
+
+// I tried to test the App.vue component, but I didn't get it to work.
+// I will learn how to test components with vitest and test the App.vue component later.
